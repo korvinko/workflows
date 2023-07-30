5f5edb7a0eeebdcea9541ec8626f459e5eedcdd1
korvinko <john.doe.jtg@gmail.com>
2023-07-30 11:59:03 +0200

    Ignore non-working days when gets rates according to this information:
    https://nbp.pl/en/statistic-and-financial-reporting/rates/dates-of-nbp-currency-exchange-rates-publication/

--- integrations/nbpl/rates.ts
+++ integrations/nbpl/rates.ts
@@ -1,7 +1,9 @@
 import fs from 'fs';
+import moment from 'moment';
 
 export async function getCurrencyBuyRate(currency: string): Promise<any> {
-	const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/c/${currency}/today/?format=json`);
+	const date = getWorkingDayDate().format('YYYY-MM-DD');
+	const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/c/${currency}/${date}/?format=json`);
 	const data = await response.json();
 
 	return data.rates[0].bid;
@@ -20,6 +22,19 @@ export async function currencies(): Promise<any> {
 	} catch (err) {
 		console.error('Error:', err);
 	}
+}
+
+function getWorkingDayDate(): moment.Moment {
+	let date = moment();
+
+	// If it's Saturday or Sunday, set the date to the last Friday
+	if (date.day() === 0) { // Sunday
+		date = moment().subtract(2, 'days');
+	} else if (date.day() === 6) { // Saturday
+		date = moment().subtract(1, 'days');
+	}
+
+	return date;
 }
 
 
